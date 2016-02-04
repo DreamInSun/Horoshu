a7eb5f37dc4a5d8e95cc8e69d58c4f1b8425249f
DreamInSun <yancy_chen@hotmail.com>
2016-02-05 02:27:34 +0800

    HttpReqChain Http链式请求类完成。

---
+++ Horoshu-Core/src/main/java/orange/core/horoshu/http/HttpRequest.java
@@ -0,0 +1,49 @@
+package orange.core.horoshu.http;
+
+import org.apache.http.client.methods.*;
+
+/**
+ * Created by DreamInSun on 2016/2/5.
+ */
+public class HttpRequest extends HttpEntityEnclosingRequestBase {
+
+    /*========== Constant ==========*/
+     /* REquest Method */
+    public static final String METHOD_GET = HttpGet.METHOD_NAME;
+    public static final String METHOD_POST = HttpPost.METHOD_NAME;
+    public static final String METHOD_PUT = HttpPut.METHOD_NAME;
+    public static final String METHOD_DELETE = HttpDelete.METHOD_NAME;
+   /* More Method : CONNECT,OPTIONS, PATCH, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK */
+
+    /* Content-Type */
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String CONTENT_TYPE_XML = "application/xml";
+    public static final String CONTENT_TYPE_PLAIN = "text/plain";
+    public static final String CONTENT_TYPE_MULTIPART = "multipart/form-data";
+
+    /*========== Properties ==========*/
+    private String m_method = HttpRequest.METHOD_GET;
+
+    /*========== Constructor ==========*/
+    public HttpRequest() {
+        super();
+    }
+
+    /*========== Override ==========*/
+    @Override
+    public String getMethod() {
+        return m_method;
+    }
+
+    /*========== Export Function ==========*/
+    public void setMethod(String method) {
+        switch (method) {
+            case HttpRequest.METHOD_DELETE:
+            case HttpRequest.METHOD_PUT:
+            case HttpRequest.METHOD_POST:
+            case HttpRequest.METHOD_GET:
+                m_method = method;
+                break;
+        }
+    }
+}
